02cf6244682e3c4ec06661da1539ae92fcdf3707
MD Shamim <mshamimansari111.com>
2020-10-09 00:39:46 +0530

    add;

--- config/appConfig.js
+++ config/appConfig.js
@@ -3,7 +3,8 @@ appConfig.port = 8081;
 appConfig.env = "dev";
 appConfig.db = {
   // uri: "mongodb://127.0.0.1:27017/todoAppDB",
-  uri:"mongodb+srv://cluster0.1cib5.mongodb.net/todoAppDB"
+  // uri:"mongodb+srv://admin:<password>@cluster0.1cib5.mongodb.net/todoAppDB"
+  uri:"mongodb+srv://admin:Password123@cluster0.1cib5.mongodb.net/test"
 };
 appConfig.aviVersion = "/api/v1";
 appConfig.allowedCorsOrigin = "*";
