08f27389cebb02231948f2e2875dd965a7f7673c
Md Shamim <Shamim@dotcod.in>
2020-11-21 23:35:34 +0530

    add

--- client/app1.js
+++ client/app1.js
@@ -1,92 +1,85 @@
-const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqd3RpZCI6Iks4YTEtd3dNVCIsImlhdCI6MTYwMjA5ODk2NTIyMCwiZXhwIjoxNjAyMTg1MzY1LCJzdWIiOiJhdXRoVG9rZW4iLCJpc3MiOiJ0b2RvQXBwIiwiZGF0YSI6eyJ1c2VySWQiOiJiSWRrS203bWQiLCJmaXJzdE5hbWUiOiJzaGFtaW0xIiwibWlkZGxlTmFtZSI6IiIsImxhc3ROYW1lIjoiQWhtZWQiLCJtb2JpbGVOdW1iZXIiOjgwNzQ5OTk5MjMsImdlbmRlciI6Im1hbGUiLCJhcGlLZXkiOiIxMjM0NTYiLCJlbWFpbCI6InNoYW1pbTFAZ21haWwuY29tIn19.4f9sDX_7YrmcvSBXvgUYp82FT-WH9uPXj8YGie2g1os"
-const secretKey ="mySecretKeyThatNoOneKnowsInHisWildDream"
-const userId = 'bIdkKm7md'
-const socket = io('http://localhost:8081')
+const authToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqd3RpZCI6Iks4YTEtd3dNVCIsImlhdCI6MTYwMjA5ODk2NTIyMCwiZXhwIjoxNjAyMTg1MzY1LCJzdWIiOiJhdXRoVG9rZW4iLCJpc3MiOiJ0b2RvQXBwIiwiZGF0YSI6eyJ1c2VySWQiOiJiSWRrS203bWQiLCJmaXJzdE5hbWUiOiJzaGFtaW0xIiwibWlkZGxlTmFtZSI6IiIsImxhc3ROYW1lIjoiQWhtZWQiLCJtb2JpbGVOdW1iZXIiOjgwNzQ5OTk5MjMsImdlbmRlciI6Im1hbGUiLCJhcGlLZXkiOiIxMjM0NTYiLCJlbWFpbCI6InNoYW1pbTFAZ21haWwuY29tIn19.4f9sDX_7YrmcvSBXvgUYp82FT-WH9uPXj8YGie2g1os";
+const secretKey = "mySecretKeyThatNoOneKnowsInHisWildDream";
+const userId = "bIdkKm7md";
+const socket = io("http://localhost:8081");
 
 let clientJs = () => {
+  // add
+  let userInfo = {
+    senderId: userId,
+    senderName: "shamim1",
+    receiverName: "shamim1",
+    receiverId: "Wk32x_gFz",
+  };
 
-    let userInfo = {
-        senderId: userId,
-        senderName: 'shamim1',
-        receiverName: 'shamim1',
-        receiverId: 'Wk32x_gFz',
-    }
- 
-    let chatMessage = {
-        senderId: userId,
-        senderName: 'shamim1 Ahmed',
-        receiverName: 'shamim1 Ahmed',
-        receiverId: 'Wk32x_gFz',
-        createdOn: Date.now()
-    }
+  let chatMessage = {
+    senderId: userId,
+    senderName: "shamim1 Ahmed",
+    receiverName: "shamim1 Ahmed",
+    receiverId: "Wk32x_gFz",
+    createdOn: Date.now(),
+  };
 
+  socket.on("verifyUser", (data) => {
+    console.log("Server wants to verify the user");
 
-    socket.on('verifyUser', (data) => {
+    //sending authtoken to setUser event
+    socket.emit("setUser", authToken);
+  });
 
-        console.log('Server wants to verify the user')
+  socket.on(userId, (data) => {
+    //console.log(data)
+    console.log("You have received a message from  " + data.senderName);
+    console.log(data.message);
+  });
 
-        //sending authtoken to setUser event
-        socket.emit('setUser', (authToken))
-    })
+  socket.on(userInfo.senderName, (userInfo) => {
+    //console.log(userInfo)
+    console.log(userInfo.senderName + "  is typing...");
+  });
 
-    socket.on(userId, (data) => {
-        //console.log(data)
-        console.log("You have received a message from  " + data.senderName)
-        console.log(data.message)
-    })
+  socket.on("auth-error", (data) => {
+    console.log(data);
+  });
 
-    socket.on(userInfo.senderName, (userInfo) => {
-        //console.log(userInfo)
-        console.log(userInfo.senderName + '  is typing...')
-    })
+  socket.on("all-onlineUsersList", (list) => {
+    console.log("users list updated");
+    console.log(list);
+  });
 
-    socket.on('auth-error', (data) => {
-        console.log(data)
-    })
+  socket.on("come", (fullname) => {
+    console.log(fullname + "  came online");
+  });
 
-    socket.on('all-onlineUsersList', (list) => {
-        console.log('users list updated')
-        console.log(list)
-    })
+  socket.on("leave", (fullname) => {
+    console.log(fullname + "  go offline");
+  });
 
-    socket.on('come', (fullname) => {
-        console.log(fullname + '  came online')
-    })
+  socket.on("message", (data) => {
+    console.log(data.senderName + "  says: " + data.groupMessage);
+  });
 
-    socket.on('leave', (fullname) => {
-        console.log(fullname + '  go offline')
-    })
+  socket.on("notify", (data) => {
+    console.log(data + "  is clicked a button on his side");
+  });
 
-    socket.on('message', (data) => {
-        console.log(data.senderName + '  says: ' + data.groupMessage)
-    })
+  $("#send1").on("click", function () {
+    let messageText = $("#messageToSend").val();
+    chatMessage.message = messageText;
+    socket.emit("chat-msg", chatMessage);
+  });
 
-    socket.on('notify', (data) => {
-        console.log(data + "  is clicked a button on his side")
-    })
+  $("#send2").on("click", function () {
+    let groupMessageText = $("#groupChat").val();
+    chatMessage.groupMessage = groupMessageText;
+    socket.emit("group-chat", chatMessage);
+  });
 
-    $("#send1").on('click', function () {
-
-        let messageText = $("#messageToSend").val()
-        chatMessage.message = messageText;
-        socket.emit("chat-msg", chatMessage)
-
-    })
-
-    $("#send2").on('click', function () {
-
-        let groupMessageText = $("#groupChat").val()
-        chatMessage.groupMessage = groupMessageText;
-        socket.emit("group-chat", chatMessage)
-
-    })
-
-    $("#messageToSend").keypress(function () {
-
-        socket.emit("typing", userInfo)
-
-    })
-}
+  $("#messageToSend").keypress(function () {
+    socket.emit("typing", userInfo);
+  });
+};
 
 //calling clientJs function
 clientJs();
